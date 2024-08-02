a7effd9249a8aa60441d1de9278d181fe7fbeb6d
WesleyDoria <110678889+Wesleygabrieldoria@users.noreply.github.com>
2024-08-02 07:45:14 -0300

    Add files via upload

---
+++ IA3DW-main/IA3DW/script.js
@@ -0,0 +1,13 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alltrnativas:[
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    }
+]
