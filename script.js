14921f8254aedd6ef7c63988c48ed4da14b226ea
WesleyDoria <110678889+Wesleygabrieldoria@users.noreply.github.com>
2024-08-08 08:32:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const caixaPrincipal = document.querySelector('.caixa-principal');
-const caixaPergunta = document.querySelector('.caixa-pergunta');
-const caixaAlternativa = document.querySelector('.caixa-alternativa');
-const caixaResultado = document.querySelector('.caixa-resultado');
-const perguntas = [
+const caixaPrincipal=document.querySelector('caixa-principal');
+const caixaPergunta=document.querySelector('caixa-pergunta');
+const caixaResultado=document.querySelector('caixa-resultado');
+const caixaAlternativa=document.querySelector('caixa-alternativa');
+
+const pergunta=[
     {
-        enunciado: "Pergunta 1",
-        alltrnativas:[
-            "Alternativa 1",
-            "Alternativa 2",
-        ]
-    }
+        enunciado:"Pergunta 1",
+        Alternativas:[ "Alternativa 1", "Alternativa 2" ]
+    },
+{
+    enunciado:"Pergunta 2",
+    Alternativas:[ "Alternativa 1", "Alternativa 2" ]
+},
+{
+    enunciado:"Pergunta 3",
+    Alternativas:[ "Alternativa 1", "Alternativa 2" ]
+},
 ]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    
+}
