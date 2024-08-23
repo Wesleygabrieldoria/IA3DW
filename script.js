66c96164aaf8157c1842fec95b1f865b35b63265
WesleyDoria <110678889+Wesleygabrieldoria@users.noreply.github.com>
2024-08-23 07:47:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-const caixaPrincipal=document.querySelector('caixa-principal');
-const caixaPergunta=document.querySelector('caixa-pergunta');
-const caixaResultado=document.querySelector('caixa-resultado');
-const caixaAlternativa=document.querySelector('caixa-alternativa');
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
 
-const pergunta=[
+const perguntas = [
     {
-        enunciado:"Pergunta 1",
-        Alternativas:[ "Alternativa 1", "Alternativa 2" ]
+        enunciado: "Pergunta 1",
+        alternativas:["Alternativa 1","Alternativa 2"]
     },
-{
-    enunciado:"Pergunta 2",
-    Alternativas:[ "Alternativa 1", "Alternativa 2" ]
-},
-{
-    enunciado:"Pergunta 3",
-    Alternativas:[ "Alternativa 1", "Alternativa 2" ]
-},
+    {
+        enunciado: "Pergunta 2",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    }
 ]
 
 let atual = 0;
 let perguntaAtual;
 
 function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPergunta.textContent = perguntaAtual.enunciado;
     
 }
