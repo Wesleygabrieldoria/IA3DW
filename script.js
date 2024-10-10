1db86a61cb198425567e031ddc09249973fc4887
WesleyDoria <110678889+Wesleygabrieldoria@users.noreply.github.com>
2024-10-10 08:36:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
 
 const perguntas = [
     {
@@ -47,29 +48,35 @@ const perguntas = [
 
 let posicao = 0;
 let perguntaAtual;
-let resposta;
+let respostas = "";
 
 function mostraPergunta() {
+    if (posicao >= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[posicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
+    caixaAlternativa.textContent = " ";
     mostraAlternativas();
 }
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa)
-            
-        );
+        botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
         caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionaa.afirmativa;
-    respostas = afirmacoes;
+    const afirmacoes = opcaoSelecionada.afirmativa;
+    respostas += afirmacoes;
     posicao++;
-    mostraPergunta;
-    
+    mostraPergunta();
+}
+function mostraResultado(){
+    caixaPergunta.textContent = "Em 25 anos...";
+    textoResultado.textContent = respostas;
 
 }
 mostraPergunta();
