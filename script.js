b7a8679b850a61237d496ba212c46bea5ee5d1fc
WesleyDoria <110678889+Wesleygabrieldoria@users.noreply.github.com>
2024-10-11 07:51:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,46 +4,34 @@ const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
 
-const perguntas = [
-    {
-        enunciado: "Você gosta da ideia de Inteligência Artificial?",
+const perguntas = [     //serve para abrir lista de perguntas
+    {   //abre o objeto das perguntas
+        enunciado: "Você acha que IA vai ser um problema no futuro?",
         alternativas: [
-            {
-                texto: "Sim",
-                afirmativa: "Afirmativa da alternativa 1"
-            },
-            {
-                texto: "Não",
-                afirmativa: "Afirmativa da alternativa 2"
-            }
-        ]
+            {texto: "Sim",
+            afirmação:"A IA vai ser um problema no futuro"}, 
+
+            {texto: "Não",
+            afirmação:"A IA não vai ser um problema no futuro"}]
     },
-    {
-        enunciado: "Pergunta 2",
+    { 
+        enunciado: "Você acredita que a IA vai ter vida própria?",
         alternativas: [
-            {
-                texto: "Sim",
-                afirmativa: "Afirmativa da alternativa 1"
-            },
-            {
-                texto: "Não",
-                afirmativa: "Afirmativa da alternativa 2"
-            }
-        ]
+            {texto: "Sim",
+            afirmação:"A IA terá vida própria"}, 
+                
+            {texto: "Não",
+            afirmação:"A IA não terá vida própria"}]
     },
-    {
-        enunciado: "Pergunta 3",
+    { 
+        enunciado: "Qualquer um pode usar a IA?",
         alternativas: [
-            {
-                texto: "Sim",
-                afirmativa: "Afirmativa da alternativa 1"
-            },
-            {
-                texto: "Não",
-                afirmativa: "Afirmativa da alternativa 2"
-            }
-        ]
-    }
+            {texto: "Sim",
+            afirmação:"Sim, qualquer um pode usar a IA"}, 
+                
+            {texto: "Não",
+            afirmação:"Não, qualquer um não pode usar a IA"}]
+    },
 ]
 
 let posicao = 0;
@@ -69,14 +57,14 @@ function mostraAlternativas() {
     }
 }
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmativa;
-    respostas += afirmacoes;
+    const afirmacoes = opcaoSelecionada.afirmação;
+    respostas += afirmacoes + " ";
     posicao++;
     mostraPergunta();
 }
 function mostraResultado(){
     caixaPergunta.textContent = "Em 25 anos...";
     textoResultado.textContent = respostas;
-
+    caixaAlternativa.textContent = "";
 }
 mostraPergunta();
